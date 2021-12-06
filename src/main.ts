d94bfa9a059457be0c0e50c326fd31d04ea38a12
aims-seony <seony1107@aimskorea.kr>
2021-12-07 08:15:59 +0900

    dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true,
+    forbidNonWhitelisted:true,
+    transform:true, // 원하는 실제 타입으로 변경
+  }));
   await app.listen(3000);
 }
 bootstrap();
